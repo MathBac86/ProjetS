47386e892ee126e9fb5c8ae496f8eb64dbcff9c0
MatBac <matbaconnet@gmail.com>
2024-09-27 12:29:09 +0200

    datatable html+css ok / js part1

--- assets/app.js
+++ assets/app.js
@@ -15,12 +15,70 @@ import './styles/input.css';
 
 import './styles/admin/admin.css';
 import './styles/admin/sidebar.css';
+import './styles/admin/table.css'
 
 import 'bootstrap';
-import 'datatables.net';
+import DataTable from 'datatables.net';
+new DataTable('#example', {
+    aaSorting: [],
+    pagingType: 'full_numbers',
+    buttons: [
+       {
+        extend: 'excelHtml5',
+        exportOptions: {
+            columns: [ 0, 1, 2, 3, 4, 5, 6 ]
+        }
+       },
+      {
+        extend: 'pdfHtml5',
+        exportOptions: {
+            columns: [ 0, 1, 2, 3, 4, 5, 6 ]
+        }
+      }
+    ],
+    responsive: true,
+    language: {
+        "search": "Recherche : ",
+        "lengthMenu": "Seulement : _MENU_ entrées/page",
+        "info":  "Du _START_ au _END_ sur _TOTAL_ entrées",
+        "paginate": {
+            "first":      '<i class="fa-solid fa-angles-left fa-xs mb-3"></i>',
+            "last":       '<i class="fa-solid fa-angles-right fa-xs mb-3"></i>',
+            "next":       '<i class="fa-solid fa-angle-right fa-3xs mt-2"></i>',
+            "previous":   '<i class="fa-solid fa-angle-left fa-2xs mt-2"></i>'
+        },
+        "emptyTable": "Pas d'user disponible",
+        "infoFiltered": " - filtré sur _MAX_ enregistrements",
+        "zeroRecords":    "Aucun résultat trouvé",
+        "infoEmpty":      "Aucune réponse",
+    },
+    columnDefs: [
+        {
+          responsivePriority: 1,
+          targets: 0
+        },
+        {
+          responsivePriority: 2,
+          targets: -1
+        }
+      ]
+} );
+   
+document.querySelector('#example').buttons().container()
+    .appendTo( '#example_wrapper .col-md-6:eq(0)' );
+
+var search = $document.querySelector(".dataTables_filter input")
+    .attr("placeholder", "Votre recherche")
+    .css({
+        width: "250px",
+        display: "inline-block"
+    });
+
+
 import 'jquery'
 import './js/menu/menu.js';
 
 import './js/admin/sidebar.js';
+import './js/admin/table.js'
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
